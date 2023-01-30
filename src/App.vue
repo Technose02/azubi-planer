a7a058ad8401b364de8652e6c728fb300fcd7cb1
Niklas Fischer-von Roenn <technose02@yahoo.de>
2023-01-30 07:33:16 +0100

    Year in App über Variable parametriert (vereinfacht das Testen deutlich)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
-<script>
-import Planner from "./components/Planner/Planner.vue";
-import PlannerEntry from "./components/Planner/PlannerEntry.vue";
-
-export default {
-  components: {
-    Planner,
-    PlannerEntry,
-  },
-};
-</script>
-
 <template>
   <header>
-    <h1>Planungsübersicht</h1>
+    <h1>Azubi-Blockplanung für {{ this.year }}</h1>
   </header>
 
   <main>
@@ -27,47 +15,47 @@ export default {
           { title: 'Ilse Inzidenz', key: 'iilse' },
           { title: 'Dennis Decrypter', key: 'ddennis' },
         ]"
-        :year="2023"
+        :year="this.year"
       >
         <planner-entry
           :row-keys="['ffarina', 'ssebastian', 'iingo']"
-          :start-date="new Date(2023, 1, 1)"
-          :end-date="new Date(2023, 1, 28)"
+          :start-date="new Date(this.year, 1, 1)"
+          :end-date="new Date(this.year, 1, 28)"
           color="#0FF"
           >Anwendungsentwicklung</planner-entry
         >
         <planner-entry
           :row-keys="['ssebastian', 'iingo']"
-          :start-date="new Date(2023, 0, 23)"
-          :end-date="new Date(2023, 0, 31)"
+          :start-date="new Date(this.year, 0, 23)"
+          :end-date="new Date(this.year, 0, 31)"
           color="#AC8"
           >UHD</planner-entry
         >
         <planner-entry
           :row-keys="['vvigo', 'ddanzo', 'iilse']"
-          :start-date="new Date(2023, 0, 30)"
-          :end-date="new Date(2023, 1, 19)"
+          :start-date="new Date(this.year, 0, 30)"
+          :end-date="new Date(this.year, 1, 19)"
           color="#FF0"
           >Berufschule</planner-entry
         >
         <planner-entry
           :row-keys="['ddennis']"
-          :start-date="new Date(2023, 1, 1)"
-          :end-date="new Date(2023, 3, 30)"
+          :start-date="new Date(this.year, 1, 1)"
+          :end-date="new Date(this.year, 3, 30)"
           color="#9BE"
           >Abschlussprojekt</planner-entry
         >
         <planner-entry
           :row-keys="['ffarina']"
-          :start-date="new Date(2023, 0, 2)"
-          :end-date="new Date(2023, 0, 4)"
+          :start-date="new Date(this.year, 0, 2)"
+          :end-date="new Date(this.year, 0, 4)"
           color="#08F"
           >PJM</planner-entry
         >
         <planner-entry
           :row-keys="['ffarina']"
-          :start-date="new Date(2023, 11, 25)"
-          :end-date="new Date(2023, 11, 31)"
+          :start-date="new Date(this.year, 11, 25)"
+          :end-date="new Date(this.year, 11, 31)"
           color="#08F"
           >PJM</planner-entry
         >
@@ -75,6 +63,21 @@ export default {
     </div>
   </main>
 </template>
+<script>
+import Planner from "./components/Planner/Planner.vue";
+import PlannerEntry from "./components/Planner/PlannerEntry.vue";
+export default {
+  data() {
+    return {
+      year: 2023,
+    };
+  },
+  components: {
+    Planner,
+    PlannerEntry,
+  },
+};
+</script>
 
 <style scoped>
 h1 {
